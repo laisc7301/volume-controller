ef36e1b1cc011d499dec7887d579b718737d61b3
laisc(home) <laisc7301@gmail.com>
2024-01-15 06:41:54 +0800

    n

---
+++ volume-controller1.5(test)/content_scripts/controller.js
@@ -0,0 +1,74 @@
+(function() {
+
+	if (window.hasRun) {
+		return;
+	}
+	window.hasRun = true;
+
+	var nowvolume = 1;
+	var volumearray = new Array();
+
+	browser.runtime.onMessage.addListener((message) => {
+		if (message.command == "setvolume") {
+			var volumeFloat = parseFloat(message.volume);
+			nowvolume *= volumeFloat;
+			volumearray.push(volumeFloat);
+			setVolume(volumeFloat);
+		};
+
+		if (message.command == "resetvolume") {
+			if (nowvolume != 1) {
+				
+				for(i=volumearray.length-1;i>=0;i--){
+					setVolume(-volumearray[i]);
+				}
+				
+				
+				setVolume(1);
+			}
+			volumearray.splice(0,volumearray.length);
+
+			nowvolume = 1;
+		}
+		// if (message.command == "resetvolumetest") {
+			
+		// 		setVolume(0);
+			
+		// }
+	});
+
+	function setVolume(volume) {
+
+		const AudioContext = window.AudioContext || window.webkitAudioContext;
+		var audioContexts = new Array();
+
+		var audioElements = document.querySelectorAll("video,audio");
+
+		for (let i = 0; i < audioElements.length; i++) {
+			audioContexts[i] = new AudioContext();
+		}
+
+		var tracks = new Array();
+		for (let i = 0; i < audioElements.length; i++) {
+			tracks[i] = audioContexts[i].createMediaElementSource(audioElements[i]);
+		}
+
+		var gainNodes = new Array();
+		for (let i = 0; i < audioContexts.length; i++) {
+			gainNodes[i] = audioContexts[i].createGain();
+		}
+
+		for (let i = 0; i < tracks.length; i++) {
+			tracks[i].connect(gainNodes[i]).connect(audioContexts[i].destination);
+		}
+
+		for (let i = 0; i < tracks.length; i++) {
+			gainNodes[i].gain.value = volume;
+		}
+
+	}
+
+	
+
+
+})()
